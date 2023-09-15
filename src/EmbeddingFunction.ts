e9f65f925e6a9729efe570466a2bf9fc63e8ffac
Rahul Sharma <rahu8299@gmail.com>
2023-09-15 13:23:25 +0530

    basic class

---
+++ src/EmbeddingFunction.ts
@@ -0,0 +1,43 @@
+interface IEmbeddingFunction {
+    generate(texts: string[]): Promise<number[][]>;
+}
+export class EmbeddingFunction implements IEmbeddingFunction {
+    private api_key: string;
+    private api_path: string;
+
+    constructor({
+        api_path,
+        api_key,
+    }: {
+        api_path: string
+        api_key: string,
+    }) {
+
+        this.api_key = api_key;
+        this.api_path = api_path;
+
+    }
+
+    public async generate(texts: string[]): Promise<number[][]> {
+
+        const res = await fetch(this.api_path, {
+            method: "POST",
+            headers: { "Authorization": `Bearer ${this.api_key}` },
+            body: JSON.stringify({
+                "inputs": texts
+            })
+        }).catch(e => {
+            console.error(e)
+            throw new Error('Failed to fetch')
+        })
+
+
+        const response: any = await res.json().catch(e => {
+            console.error(e)
+            throw new Error('unknown response')
+        })
+        // console.log(response);
+
+        return response;
+    }
+}
